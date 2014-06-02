53ac420659ca4159441a95aab1cecb2b1c45c695
Thomas Shafer <thomasjshafer@gmail.com>
2014-06-02 15:32:06 -0700

    example cine.io app in node using the cine-io node package

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# cine.io example application in Node.js
+
+This is an example application in Node.js showing the capabilities of [cine.io](https://www.cine.io). It uses the [cine.io JS SDK](https://github.com/cine-io/js-sdk) and the [cine.io Node.js package](https://github.com/cine-io/cineio-node).
+
+## Usage
+
+1. `git clone https://github.com/cine-io/node-example-app.git`
+1. `cd node-example-app`
+1. Add your cine.io api keys (CINE_IO_PUBLIC_KEY and CINE_IO_PRIVATE_KEY) to your environment.
+1. `npm install`
+1. `node index.js`
+1. visit localhost:3000 in your browser
--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 var express = require('express');
 var app = express();
+var CineIO = require('cine-io');
+// CINE IO API KEYS
+var publicKey = process.env.CINE_IO_PUBLIC_KEY;
+var secretKey = process.env.CINE_IO_SECRET_KEY;
+var client = CineIO.init({secretKey: secretKey});
 
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+
+// Show the project details
 app.get('/', function(req, res){
-  res.send('Hello World');
+  client.project.get(function(err, project){
+    res.render('project_show', {title: 'Project/show', publicKey: publicKey, project: project, projectData: JSON.stringify(project)})
+  });
+});
+
+// Show all the streams
+app.get('/streams', function(req, res){
+  client.streams.index(function(err, streams){
+    res.render('streams_index', {title: 'Streams/index', publicKey: publicKey, streams: streams, streamsData: JSON.stringify(streams)})
+  });
+});
+
+// Create a new stream
+app.get('/streams/create', function(req, res){
+  client.streams.create(function(err, stream){
+    res.redirect('/stream/'+stream.id)
+  });
+});
+
+// Show a specific stream
+app.get('/stream/:id', function(req, res){
+  client.streams.get(req.params.id, function(err, stream){
+    res.render('streams_show', {title: 'Streams/show', publicKey: publicKey, id: stream.id, password: stream.password, streamData: JSON.stringify(stream)})
+  });
 });
 
 var port = process.env.PORT;
-port || port = 3000;
+port || (port = 3000);
 
 var server = app.listen(port, function() {
   console.log('Listening on port %d', server.address().port);
